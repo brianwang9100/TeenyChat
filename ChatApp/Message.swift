478484460faead108edaa4034c020986b0efb239
brianwang9100@gmail.com <brianwang9100@gmail.com>
2017-01-22 00:56:18 -0500

    ChatApp done

---
+++ ChatApp/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  ChatApp
+//
+//  Created by Brian Wang on 1/21/17.
+//  Copyright © 2017 BW. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    var text:String
+    var date:Date
+}
